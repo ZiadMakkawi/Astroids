55335a4681533e16b5f7b53e6784edab6ec56ad5
ZiadMakkawi <ziadmakkawi16@gmail.com>
2025-09-29 19:08:09 +0100

    Created the player and astroieds functionality

--- main.py
+++ main.py
@@ -1,28 +1,57 @@
 #source .venv/bin/activate
+# export DISPLAY=:0.0 
+# run this above to fix any issues related to VcXsrv
+import sys
 import pygame 
 from constants import *
+from player import *
+from asteroids import *
+from asteroidfield import *
 
 
 def main():
-    print("Starting Asteroids!")
-    print (f"Screen width: {SCREEN_WIDTH}")
-    print (f"Screen height: {SCREEN_HEIGHT}")
-    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroids.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
 
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    running = True
 
-running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        updatable.update(dt)
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
 
-    screen.fill("black")
+        screen.fill("black")
+        
+        for obj in drawable:
+            obj.draw(screen)
 
-    pygame.display.flip()
+        pygame.display.flip()
+
+        dt = clock.tick(60)/1000
+        
 
 
 
+if __name__ == "__main__":
+    main()
